f6e3ea4cb640b4efc1dad026555b5b095d97f6b7
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-16 00:09:12 -0300

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "cleaning airlines"
+Airline.destroy_all
+
+airlines = Airline.create([
+    { 
+        image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png",
+        name: "United Airlines"
+    }, 
+    { 
+        image_url: "https://open-flights.s3.amazonaws.com/Southwest-Airlines.png",
+        name: "Southwest"
+    },
+    { 
+        image_url: "https://open-flights.s3.amazonaws.com/Delta.png",
+        name: "Delta"
+    }, 
+    { 
+        image_url: "https://open-flights.s3.amazonaws.com/Alaska-Airlines.png",
+        name: "Alaska Airlines"
+    }, 
+    { 
+        image_url: "https://open-flights.s3.amazonaws.com/JetBlue.png",
+        name: "JetBlue"
+    }, 
+    { 
+        image_url: "https://open-flights.s3.amazonaws.com/American-Airlines.png",
+        name: "American Airlines"
+    }
+])
+    
+puts "Created airlines"
+
+puts "cleaning reviews"
+Review.destroy_all
+
+reviews = Review.create([
+    {
+        title: "cold food served",
+        description: "they served bad food",
+        score: 1,
+        airline: airlines.first
+    },
+    {
+        title: "amazing flight attendent",
+        description: "they are always smiling",
+        score: 5,
+        airline: airlines.first
+    }
+])
+
+puts "created reviews"
