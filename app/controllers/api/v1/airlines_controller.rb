1998c22a940b40e956382fa3a1900c42bbd8e8d5
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-16 23:41:51 -0300

    create and show method

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -1,14 +1,31 @@
 class Api::V1::AirlinesController < ApplicationController
+    before_action :authenticate, only: [:create :upate :destroy]
+    
     def index
-        @airlines = Airline.all
-
-        AirlineSerializer.new(airlines).serialized_json
+        render json: serializer(airlines, options)
     end
 
+    #GET /api/v1/:slug
     def show
+        render json: serializer(airlines, options)
+    end
+
+    def create
+        airline = Airline.new(airline_params)
+        
+        if airline.save
+            render json: serializer(airline)
+        else
+            render json: errors(airline), status: 422
+        end         
+    end
+    private
+
+    def options
+        @options ||= { include: %i[reviews] } 
+    end
+
+    def before_action
         @arline = Airline.find_by(slug:params[:slug])
-
-        AirlineSerializer.new(airline).serialized_json
-
     end
 end
