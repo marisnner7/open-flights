99aebeb172fb5d08366abc1d8ef6083fbb028eaa
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-17 00:55:00 -0300

    hot fix routes path

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :airlines, params: :slug
+      resources :airlines, param: :slug
       resources :reviews, only: [:create, :destroy]
     end
   end
 
-  get "#path", to: 'pages#index', via: :all
+  get "*path", to: 'pages#index', via: :all
 end
