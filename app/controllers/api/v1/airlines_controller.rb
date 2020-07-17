d4dbef64b5276daabe8ed0222fa71aeeefa5535c
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-17 00:55:40 -0300

    update methods

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -1,31 +1,56 @@
 class Api::V1::AirlinesController < ApplicationController
-    before_action :authenticate, only: [:create :upate :destroy]
+    protect_from_forgery with: :null_session
     
     def index
-        render json: serializer(airlines, options)
+        airlines = Airline.all
+
+        render json: AirlineSerializer.new(airlines, options).serialized_json
     end
 
-    #GET /api/v1/:slug
     def show
-        render json: serializer(airlines, options)
+        airline = Airline.find_by(slug: params[:slug])
+
+        render json: AirlineSerializer.new(airline, options).serialized_json
+
     end
 
     def create
         airline = Airline.new(airline_params)
         
         if airline.save
-            render json: serializer(airline)
+          render json: AirlineSerializer.new(airline).serialized_json
+
         else
-            render json: errors(airline), status: 422
-        end         
+            render json: { error: airline.errors.message }, status:  422
+        end
     end
+
+    def update
+        airline = Airline.find_by(slug: params[:slug])
+        if airline.update(airline_params)
+          render json: AirlineSerializer.new(airline).serialized_json
+
+        else
+            render json: { error: airline.errors.message }, status:  422
+        end
+    end
+
+    def destroy
+        airline = Airline.find_by(slug: params[:slug])
+        if airline.destroy
+            head :no_content
+        else
+            render json: { error: airline.errors.message }, status:  422
+        end
+    end
+
+
     private
-
+    def airline_params
+        params.require(:airline).permit(:name, :image_url)
+    end    
+    
     def options
-        @options ||= { include: %i[reviews] } 
-    end
-
-    def before_action
-        @arline = Airline.find_by(slug:params[:slug])
+        @options ||= { include: %i[reviews]}
     end
 end
