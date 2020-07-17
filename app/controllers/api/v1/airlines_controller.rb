ce4cb157c13456a08660a22fd8a0eaa99c9728c3
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-17 01:29:35 -0300

    hot fixes

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -16,22 +16,23 @@ class Api::V1::AirlinesController < ApplicationController
 
     def create
         airline = Airline.new(airline_params)
-        
+
         if airline.save
           render json: AirlineSerializer.new(airline).serialized_json
 
         else
-            render json: { error: airline.errors.message }, status:  422
+            render json: { error: airline.errors.messages }, status:  422
         end
     end
 
     def update
         airline = Airline.find_by(slug: params[:slug])
+        
         if airline.update(airline_params)
           render json: AirlineSerializer.new(airline).serialized_json
 
         else
-            render json: { error: airline.errors.message }, status:  422
+            render json: { error: airline.errors.messages }, status:  422
         end
     end
 
@@ -40,12 +41,14 @@ class Api::V1::AirlinesController < ApplicationController
         if airline.destroy
             head :no_content
         else
-            render json: { error: airline.errors.message }, status:  422
+            render json: { error: airline.errors.messages }, status:  422
         end
     end
 
 
     private
+
+
     def airline_params
         params.require(:airline).permit(:name, :image_url)
     end    
