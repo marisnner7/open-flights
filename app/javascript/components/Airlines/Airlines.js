71550a391b2abf126441633792aab7817dce5d9b
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-18 17:14:45 -0300

    fixing hot hot bug with useEffect

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,15 +1,17 @@
-import React, { userState, userEffect } from 'react'
-import axios from 'axios'
-
+import React, { useState, useEffect } from 'react'
+import axios  from 'axios'
 const Airlines = () => {
-  const [airlines, setAirlines] = userState([])
+    const [airlines, setAirlines] = useState([])
 
-  userEffect(() => {
-      //Get the api
-      //update Airlines
-  })
+    useEffect(()=> {
+
+        axios.get('/api/v1/airlines.json')
+        .then( resp => console.log(resp))
+        .catch( resp => console.log(resp))
+    })
+
     return (
-        <div> This is the index page  </div>
+        <div>This is the index page</div>
     )
 }
 
