ded4bb046479030c35753f77d871dfb498cc1843
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-18 20:25:45 -0300

    rules for avg score

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,15 +1,15 @@
 class Airline < ApplicationRecord
-    has_many :reviews
+  has_many :reviews
+  validates :name, presence: true, length: { maximum: 255 }
+  before_create :slugify
 
-    validates :name, presence: true, length: { maximum: 255 }
+  def slugify
+    self.slug = name.parameterize
+  end
 
-    before_create :slugify
+  def avg_score
+		return 0 unless reviews.count.positive?
+	  reviews.average(:score).round(2).to_f
+	end
 
-    def slugify
-       self.slug = name.parameterize 
-    end
-
-    def avg_score
-       reviews.average[:score].round(2).to_f
-    end
 end
