c9f8d3aeb837ff29c5e46bb63b0c1e0eed1ca8e5
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-18 16:30:27 -0300

    setting user effect and  userState

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { userState, userEffect } from 'react'
+import axios from 'axios'
 
 const Airlines = () => {
-   const [airlines, setAirlines] = userState([])
-   
-   userEffect(() => {
+  const [airlines, setAirlines] = userState([])
 
-   })
-   
+  userEffect(() => {
+      //Get the api
+      //update Airlines
+  })
     return (
         <div> This is the index page  </div>
     )
