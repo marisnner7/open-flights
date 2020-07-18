188f8a31c420412f53397b30671728f4bbf74912
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-18 15:52:47 -0300

    creating index and show page with react

---
+++ app/javascript/components/Airline/Airline.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Airline = () => {
+    return <div> This is the show page  </div>
+}
+
+export default Airline
---
+++ app/javascript/components/Airlines/Airlines.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Airlines = () => {
+    return <div> This is the index page  </div>
+}
+
+export default Airlines
