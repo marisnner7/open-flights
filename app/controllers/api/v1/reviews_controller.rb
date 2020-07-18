c8b85575d94f8659830a46c318480fc00ca5432f
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-18 13:54:55 -0300

    actions for review controller

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,4 +1,36 @@
 class Api::V1::ReviewsController < ApplicationController
     protect_from_forgery with: :null_session
 
+
+    def create
+    
+        @review = Review.new(review_params)
+        if @review.save
+            render json: ReviewSerializer.new(review).serializer_json
+        else 
+            render json: { error: review.erros.messages }, status: 422
+        end   
+    end
+
+    def destroy
+    
+        @review = Review.find_by(params[:id])
+        if @review.destroy
+            head :no_content
+        else 
+            render json: { error: review.erros.messages }, status: 422
+        end   
+    end
+
+
+
+
+    private
+
+
+    def review_params
+        params.require(:review).permit(:title, :description, :score, :airline_id )
+    end
+
+    
 end
