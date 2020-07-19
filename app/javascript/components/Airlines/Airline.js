8bd70417a05cb1bc8b942a17b4ce059302eab2d9
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-18 21:30:15 -0300

    adding style in react

--- app/javascript/components/Airlines/Airline.js
+++ app/javascript/components/Airlines/Airline.js
@@ -1,21 +1,61 @@
 import React from 'react'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Card = styled.div`
+    border: 1px solid #efefef;
+    background: #FFF;
+    text-align: center;
+
+`
+const AirlineLogo = styled.div`
+    height: 50px;
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+    padding-top: 10px;
+    
+    img{
+        height: 50px;
+        width: 50px;
+        border-radius: 100%;
+        border: 1px solid #efefef;
+    }
+`
+const AirlineName = styled.div`
+    padding: 20px 0 10px 0;
+`
+const LinkWrapper = styled.div`
+    margin: 30px 0 20px 0;
+    height: 50px;
+        a{
+            color: #FFF;
+            background-color: #000;
+            border-radius: 4px;
+            padding: 10px 50px;
+            border: 1px solid #FFF;
+            text-decoration: none;
+        }
+    
+`
+
+
 
 const Airline = (props) => {
     return(
-        <div className="card">
-            <div className="airline-logo">
+        <Card>
+            <AirlineLogo>
                 <img src={props.attributes.image_url} alt={props.attributes.name}/>
-            </div>
-            <div className="airline-name">
-                {props.attributes.name}
-            </div>
-            <div className="score">
-                {props.attributes.avg_score}
-            </div>
-            <div className="link">
-                <a href={`/airlines/${props.attributes.slug}`}>View</a>
-            </div>
-        </div>
+            </AirlineLogo>
+                <AirlineName>
+                    {props.attributes.name}
+                    {props.attributes.avg_score}
+                </AirlineName>
+                <LinkWrapper>
+                    <Link to={"/airlines/" + props.slug}>View Here</Link>
+                </LinkWrapper>
+
+        </Card>
     )
 }
 
