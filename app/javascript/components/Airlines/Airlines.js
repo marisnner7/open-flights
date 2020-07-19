712dd9f9a800e258040ed3b9f601db1c1ae5bea6
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-19 18:55:52 -0300

    hot fix um ul list

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
 
 const Airlines = () => {
+  const [airlines, setAirlines] = useState([])
+
+  useEffect(() => {
+   
+    axios.get('/api/v1/airlines.json')
+    .then(resp => {
+      setAirlines(resp.data.data) 
+    })
+    .catch(resp => console.log(resp))
+  }, [airlines.length])
+
+  const list = airlines.map( item => {
+    return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+  })
+
   return(
-    <div>This is the Airlines#index page for our app.</div>
+    <Fragment>
+      <div className="home">
+        <div className="header">
+          <h1>OpenFlights</h1>
+          <p className="subheader">Honest, unbiased airline reviews. Share your experience.</p>
+        </div>
+        <div className="grid">
+          AIRLINES GRID GOES HERE
+          <ul>{list}</ul>
+        </div>
+    </div>
+    </Fragment>
   )
 }
 
