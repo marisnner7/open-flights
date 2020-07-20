fa4a52c3b74489015b16385e0b7c0c852330f068
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-20 14:39:55 -0300

    created airline single page

---
+++ app/javascript/components/Airlines/Airline.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
+
+
+const Airline = (props) => {
+  const {name, image_url, slug} = props.attributes
+
+  return(
+    <div className="card">
+
+      <div className="airline-logo">
+        <img src={image_url} alt={name} width="50" />
+      </div>
+
+
+      <div className="airline-name">
+        {name}
+      </div>
+
+
+      <div className="link-wrapper">
+        <Link to={"/" + slug}>View Airline</Link>
+      </div>
+
+    </div>
+  )
+}
+
+export default Airline
