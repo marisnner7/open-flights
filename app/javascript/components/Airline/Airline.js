2eb89eb29772cd093f1c5d7a5b9aa8fd5510eb66
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-23 16:12:51 -0300

    function and style for airline

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,10 +1,55 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+import Header from './Header'
+import Review from './Review'
+
+const Column = styled.div`
+  background: #fff; 
+  max-width: 50%;
+  width: 50%;
+  float: left; 
+  height: 100vh;
+  overflow-x: scroll;
+  overflow-y: scroll; 
+  overflow: scroll;
+`
+
+const Airline = (props) => {
+  const [airline, setAirline] = useState({})
+  const [review, setReview] = useState({})
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(()=> {
+    const slug = props.match.params.slug
+    const url = `/api/v1/airlines/${slug}`
+    
+    axios.get(url)
+    .then( resp => {
+      setAirline(resp.data)
+      setLoaded(true)
+    })
+    .catch( resp => console.log(resp))
 
 
-const Airline = () => {
+  },[])
+
   return(
-    <div> olha eu aqui</div>
-
+    <div className="wrapper">
+        <Column>
+          {
+          loaded && 
+            <Header
+              attributes={airline.data.attributes}
+              reviews={airline.included}
+            />
+          }
+          <div className="review">new review</div>
+        </Column>
+        <Column>
+         [review form here]
+        </Column>
+    </div>
   )
 }
 
