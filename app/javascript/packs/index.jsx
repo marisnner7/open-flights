81499cab119b27c9c7f4274427bccd8fe64a2f57
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-23 13:50:00 -0300

    styling

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -11,7 +11,6 @@ import App from '../components/App'
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Router>
-
       <Route path="/" component={App} />
     </Router>,
     document.body.appendChild(document.createElement('div')),
