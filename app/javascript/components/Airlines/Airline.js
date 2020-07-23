4cb7c80bbfdf0dd4f0afd1968bb1ab38498ed73d
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-23 17:58:26 -0300

    imported rating

--- app/javascript/components/Airlines/Airline.js
+++ app/javascript/components/Airlines/Airline.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Link } from 'react-router-dom'
 import styled from 'styled-components'
+import Rating from '../Rating/Rating'
 
 const Card = styled.div`
   border: 1px solid #efefef;
