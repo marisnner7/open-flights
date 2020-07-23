7a70eec6414ee3c80e6f02bf1cf369cbfc675223
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-23 17:58:13 -0300

    styling header

--- app/javascript/components/Airline/Header.js
+++ app/javascript/components/Airline/Header.js
@@ -1,25 +1,47 @@
 import React from 'react'
 import styled from 'styled-components'
+import Rating from '../Rating/Rating'
 
-const Head = styled.div`
-  padding: 100px 100px 10px 100px;
+const Wrapper = styled.div`
+  padding: 50px 100px 50px 0;
   font-size: 30px;
-  text-align: center;
+  img{
+    height: 50px;
+    width: 50px;
+    border-radius: 100%;
+    border: 1px solid rgba(0,0,0,0.1);
+    margin-botton: -8px;
+  }
+
 `
+const TotalReview = styled.div`
+  font-size: 18px;
+  padding: 10px 0;
+`
+
+const TotalOutOf = styled.div`
+font-size: 18px;
+font-weight: bold;
+padding: 10px 0;
+`
+
+
 const Header = (props) => {
   const { name, image_url, avg_score} = props.attributes
   const total = props.reviews.length
+  
+  
   return (
-    <div className="wrapper">
-      <Head>
+    <Wrapper>
         <img src={image_url} alt={name} />
         <h1>{name}</h1>
-      </Head>      <div>
-      <div className="totalReviews">{total} User Reviews</div>
-        <div className="starRating"></div>
-      <div className="totalOutOf">{avg_score} of 5 Stars</div>
+      <div>
+        <TotalReview>{total} User Reviews</TotalReview>
+          <div className="starRating"></div>
+        <TotalOutOf>{avg_score} of 5 Stars</TotalOutOf>
       </div>
-    </div>
+    </Wrapper>
+ 
 
   )
 }
