9216d72c392eeb8e182e628a492fde13e309f91b
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-23 13:50:27 -0300

    debbuging weird behaviour

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
-import Airline from './Airline'
 import styled from 'styled-components'
+import Airline from './Airline'
 
 
 const Home = styled.div`
   text-align:center;
-` 
+`
 const Header = styled.div`
   padding:100px 100px 10px 100px;
-  
+
   h1 {
     font-size:42px;
   }
@@ -55,5 +55,32 @@ const Airlines = () => {
     </Home>
   )
 }
+
+// export default Airlines
+
+
+// const Airlines = () => {
+//   const [airlines, setAirlines] = useState([])
+
+//   useEffect(()=> {
+//     axios.get('/api/v1/airlines.json')
+//     .then( resp => {
+//       setAirlines(resp.data.data)
+//     })
+//     .catch( resp => console.log(resp))
+//   }, [])
+
+//   const list = airlines.map( item => {
+//   return ( <li key={item.attributes.name}>{item.attributes.name}</li>)
+//   })
+
+//   return(
+//     <Fragment>
+//       <div> This is the index page</div>
+//       <ul>{list}</ul>
+
+//     </Fragment>
+//   )
+// }
 
 export default Airlines
